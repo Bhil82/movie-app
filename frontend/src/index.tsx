d54486f4abcbe3b64cca6eea0ac79e6b2b0ea0a7
bhil82 <bi-ratan@hotmail.com>
2022-04-10 10:33:59 -0300

    corrigindo_arq

---
+++ frontend/src/index.tsx
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import React from 'react';
+
+ReactDOM.render(
+    <React.StrictMode>
+          <App />
+    </React.StrictMode>,
+ document.getElementById('root'));
+
